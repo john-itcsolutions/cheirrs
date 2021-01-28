498779d72afb80173e9618bedfc0060a3c89cfab
John Olsen <john.l.olsen1958@gmail.com>
2021-01-29 08:04:40 +1100

    added k8s.yaml bundle export

--- __init__.py
+++ __init__.py
@@ -13,6 +13,7 @@ import logging
 # and remove all headers in the output
 # using empty format=''
 logging.basicConfig(filename='models.py', level=logging.DEBUG, format='')
+
 # Create connection string & engine
 db_uri = "postgresql+psycopg2://postgres:Buddha10@10.186.165.230:5432/general"
 engine = create_engine(db_uri, echo=False)
@@ -20,26 +21,95 @@ engine = create_engine(db_uri, echo=False)
 meta = MetaData()
 
 # Performs database schema inspection
-insp = reflection.Inspector.from_engine(engine)
+insp = Inspector.from_engine(engine)
 
 schemas = inspector.get_schema_names()
 
-print("The Schemas are: %s" % schemas)
+class BaseModel(db.Model):
 
-with contextlib.redirect_stdout(open('models.py','w')):
-    while True:
+    def before_save(self, *args, **kwargs):
+        pass
+
+    def after_save(self, *args, **kwargs):
+        pass
+
+    def save(self, commit=True):
+        self.before_save()
+        db.session.add(self)
+        if commit:
+            try:
+                db.session.commit()
+            except Exception as e:
+                db.session.rollback()
+                raise e
+
+        self.after_save()
+
+
+    def before_update(self, *args, **kwargs):
+        pass
+
+    def after_update(self, *args, **kwargs):
+        pass
+
+    def update(self, *args, **kwargs):
+        self.before_update(*args, **kwargs)
+        db.session.commit()
+        self.after_update(*args, **kwargs)
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        if commit:
+            db.session.commit()
+
+    @classmethod
+    def eager(cls, *args):
+        cols = [orm.joinedload(arg) for arg in args]
+        return cls.query.options(*cols)
+
+    @classmethod
+    def before_bulk_create(cls, iterable, *args, **kwargs):
+        pass
+
+    @classmethod
+    def after_bulk_create(cls, model_objs, *args, **kwargs):
+        pass
+
+
+    @classmethod
+    def bulk_create(cls, iterable, *args, **kwargs):
+        cls.before_bulk_create(iterable, *args, **kwargs)
+        model_objs = []
+        for data in iterable:
+            if not isinstance(data, cls):
+                data = cls(**data)
+            model_objs.append(data)
+
+        db.session.bulk_save_objects(model_objs)
+        if kwargs.get('commit', True) is True:
+            db.session.commit()
+        cls.after_bulk_create(model_objs, *args, **kwargs)
+        return model_objs
+
+
+    @classmethod
+    def bulk_create_or_none(cls, iterable, *args, **kwargs):
+        try:
+            return cls.bulk_create(iterable, *args, **kwargs)
+        except exc.IntegrityError as e:
+            db.session.rollback()
+            return None
+
+logging.debug('The Schemas are: %s' % schemas)
+
         for schema in schemas:
-            print("schema: %s" % schema)
-        sys.stdout.flush()
-        time.sleep(1)
+            logging.debug('schema: %s' % schema)
+        
             for table_name in inspector.get_table_names(schema=schema):
                 table = Table('table_name', meta, autoload=True, autoload_with=engine)
                 primaryKeyColName = Table.primary_key.columns.values()[0].name
-                print("table: %s" % table_name)
-            sys.stdout.flush()
-            time.sleep(1)
+                logging.debug('table: %s' % table_name)
+            
                 for column in inspector.get_columns(table_name, schema=schema):
-                    print("Column: %s" % column)
-                sys.stdout.flush()
-                time.sleep(1)
-        
+                    logging.debug('Column: %s' % column)
+                
