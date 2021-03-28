31267a7bf5bd9f00639f8f12107bc1622c161c63
john-itcsolutions <john@itcsolutions.com.au>
2021-03-29 02:08:55 +1100

    Update README.md

--- dbase_setup.sh
+++ dbase_setup.sh
@@ -2,5 +2,5 @@
 
 # dbase_setup.sh script
 
-createdb house && psql house < a_horse_backup.sql && sleep 15 && psql house < cheirrs_backup.sql && sleep 15 && psql house < cheirrs_oseer_backup.sql && sleep 15 && psql house < chubba_morris_backup.sql && sleep 15 && psql house < chubba_morris_oseer_backup.sql && sleep 15 && psql house < convey_it_backup.sql && sleep 15 && psql house < convey_it_oseer_backup.sql && sleep 15 && psql house < the_general_backup.sql && sleep 15 && psql house < the_general_oseer_backup.sql
+createdb haus && psql haus < a_horse_backup.sql && sleep 15 && psql haus < cheirrs_backup.sql && sleep 15 && psql haus < cheirrs_oseer_backup.sql && sleep 15 && psql haus < chubba_morris_backup.sql && sleep 15 && psql haus < chubba_morris_oseer_backup.sql && sleep 15 && psql haus < convey_it_backup.sql && sleep 15 && psql haus < convey_it_oseer_backup.sql && sleep 15 && psql haus < the_general_backup.sql && sleep 15 && psql haus < the_general_oseer_backup.sql
  
