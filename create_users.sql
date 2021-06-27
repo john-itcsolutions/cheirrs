9c93c584e68ef77ac2f41138fa6e4c50250802ba
john-itcsolutions <john@itcsolutions.com.au>
2021-06-27 17:47:44 +1000

    Re-introduced create_users.sql to repo.

---
+++ create_users.sql
@@ -0,0 +1,57 @@
+create role cheirrs_user with login password 'passwd';
+
+create role cheirrs_admin with superuser login password 'passwd';
+
+create role cheirrs_oseer_admin with superuser login password 'passwd';
+
+create role a_horse_admin with superuser login password 'passwd';
+
+create role chubba_morris_user with login password 'passwd';
+
+create role chubba_morris_admin with superuser login password 'passwd';
+
+create role chubba_morris_oseer_admin with superuser login password 'passwd';
+
+create role convey_it_user with login password 'passwd';
+
+create role convey_it_admin with superuser login password 'passwd';
+
+create role convey_it_oseer_admin with superuser login password 'passwd';
+
+create role the_general_user with login password 'passwd';
+
+create role the_general_admin with superuser login password 'passwd';
+
+create role the_general_oseer_admin with superuser login password 'passwd';
+
+alter role gmu with superuser login password 'gmu';
+
+GRANT USAGE ON SCHEMA a_horse TO gmu;
+
+GRANT USAGE ON SCHEMA cheirrs TO gmu;
+
+GRANT USAGE ON SCHEMA cheirrs_oseer TO gmu;
+
+GRANT USAGE ON SCHEMA chubba_morris_oseer TO gmu;
+
+GRANT USAGE ON SCHEMA chubba_morris TO gmu;
+
+GRANT USAGE ON SCHEMA convey_it_oseer TO gmu;
+
+GRANT USAGE ON SCHEMA convey_it TO gmu;
+
+GRANT USAGE ON SCHEMA iot TO gmu;
+
+GRANT USAGE ON SCHEMA the_general_oseer TO gmu;
+
+GRANT USAGE ON SCHEMA the_general TO gmu;
+
+GRANT USAGE ON SCHEMA tiger TO gmu;
+
+GRANT USAGE ON SCHEMA tiger_data TO gmu;
+
+GRANT USAGE ON SCHEMA topology TO gmu;
+
+GRANT USAGE ON SCHEMA public TO gmu;
+
+ALTER ROLE gmu SET search_path TO public, a_horse, cheirrs, cheirrs_oseer, chubba_morris, chubba_morris_oseer, convey_it, convey_it_oseer, iot, the_general, the_general_oseer, tiger, tiger_data, topology;
